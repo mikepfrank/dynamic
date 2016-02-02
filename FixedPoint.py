a9c3f4adc8e6bd311231a92eb5e38d2fa4548bc7
Mike Frank <michael.patrick.frank@gmail.com>
2016-02-02 16:57:47 -0700

    Started file FixedPoint.py
    So far just includes a header comment.

---
+++ FixedPoint.py
@@ -0,0 +1,24 @@
+#/==============================================================================
+#|                          TOP OF FILE - FixedPoint.py
+#|------------------------------------------------------------------------------
+#|
+#|      File name:      FixedPoint.py                   [Python source file]
+#|
+#|
+#|      Description:
+#|      ------------
+#|
+#|          This file defines a class called "FixedPoint" that provides
+#|          a fixed-point arithmetic data type.  At present, all instances
+#|          (objects of class FixedPoint) within a given application are
+#|          created with the same precision by default.
+#|
+#|
+#|      Python language versions:
+#|      -------------------------
+#|
+#|          * (To be) tested with Python 3.5.1 (Windows 64-bit)
+#|
+#|
+#|vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+
